1a3a017be982e62af5a56fb36fefa848df787a2c
TheGreatMcPain <sixsupersonic@gmail.com>
2020-11-27 16:49:41 -0600

    Create test program for LCD along side previous LED blink demo

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
-static const int led1_delay = 1000;
-static const int led2_delay = 500;
+#include "i2c_driver.h"
+#include "i2c_lcd_driver.h"
 
 struct led_args {
   int led_num;
@@ -23,9 +22,41 @@ static void led_task(void *args) {
   }
 }
 
+static void lcd_shift_task(void *args) {
+  uint8_t i2c = *(int *)args;
+  uint8_t pos = 0;
+
+  while (1) {
+    for (pos = 0; pos < 6; pos++) {
+      lcd_send_cmd(i2c, 0x1C); // shift to the right.
+      vTaskDelay(pdMS_TO_TICKS(350));
+    }
+
+    for (pos = 0; pos < 6; pos++) {
+      lcd_send_cmd(i2c, 0x18); // shift to the left.
+      vTaskDelay(pdMS_TO_TICKS(350));
+    }
+  }
+}
+
 int main() {
   sysclock_init();
   systick_init();
+
+  // Initialize I2C2
+  i2c_init(2, i2c_SM);
+  // Init LCD on I2C2
+  lcd_init(2);
+
+  // Write hello world to LCD
+  lcd_put_cur(2, 0, 2);
+  lcd_send_string(2, "HelloWorld");
+  lcd_put_cur(2, 1, 1);
+  lcd_send_string(2, "LCD20x4 Demo");
+  lcd_put_cur(2, 2, 6);
+  lcd_send_string(2, "BY");
+  lcd_put_cur(2, 3, 0);
+  lcd_send_string(2, "TheGreatMcPain");
 
   // Initialize GPIO Port C Pin 8
   // Mode Output Push-Pull at 50Mhz
@@ -43,10 +74,16 @@ int main() {
       (struct led_args *)malloc(sizeof(struct led_args));
 
   led1ArgsPtr->led_num = 1;
-  led1ArgsPtr->led_delay = led1_delay;
+  led1ArgsPtr->led_delay = 1000;
 
   led2ArgsPtr->led_num = 2;
-  led2ArgsPtr->led_delay = led2_delay;
+  led2ArgsPtr->led_delay = 500;
+
+  int *i2cPtr = (int *)malloc(sizeof(int));
+  *i2cPtr = 2;
+
+  xTaskCreate(lcd_shift_task, "LCD_Shift", 128, (void *)i2cPtr,
+              configMAX_PRIORITIES - 1, NULL);
 
   xTaskCreate(led_task, "LED_Blink_1", 128, (void *)led1ArgsPtr,
               configMAX_PRIORITIES - 1, NULL);
